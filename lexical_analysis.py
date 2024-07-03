a3419a52f8b0c26c3116d973e106fc0d8090dd27
davis <davis@davismcd.com>
2024-07-02 21:52:05 -0700

    updates

--- lexical_analysis.py
+++ lexical_analysis.py
@@ -1,8 +1,25 @@
 from re import search
+import unicodedata
 
 
-def detect_data(text):
-    if text.strip()
+def detect_data(unstripped_text):
+    text = unstripped_text.strip()
+    try:
+        return {
+            'type': 'number',
+            'value': float(text.strip())
+        }
+    except ValueError:
+        pass
+    # try:
+    #     return {
+    #         'type': 'number',
+    #         'value': unicodedata.numeric(text.strip())
+    #     }
+    # except ValueError or TypeError:
+    #     pass
+    #
+
 
 
 def detect_operation(text):
@@ -24,10 +41,21 @@ def detect_operation(text):
         }
     return None
 
-def lexical_analysis_expression(expression, line_num):
-    lexical_analyzed_expression = []
-    index = 0
-    while index < len(expression):
+def lexical_analysis_expression(unstripped_expression, line_num):
+    detected_data = detect_data(unstripped_expression)
+    if detected_data:
+        return [detected_data]
+    expression = unstripped_expression.strip()
+    for index in range(len(expression)):
+        detected_data = detect_data(expression[:index + 1])
+        if detected_data:
+            for index2 in range(index + 1, len(expression) + 1):
+                detected_operation = detect_operation(expression[index + 1:index2])
+                if detected_operation:
+                    other_analysed = lexical_analysis_expression(expression[index2:], line_num)
+                    if other_analysed:
+                        return [detected_data, detected_operation] + other_analysed
+    return None
 
 
 def lexical_analysis(code):
@@ -37,20 +65,24 @@ def lexical_analysis(code):
         line = line.strip()
         if line == '':
             continue
-        segment = ''
-        for char in line:
-            segment += char
-            if search(r'^ *log *\($', segment):
-                lexical_analyzed[-1].append({
-                    'type': 'log'
-                })
-                lexical_analyzed[-1].append({
-                    'type': 'open par'
-                })
-                segment = ''
-    return lexical_analyzed
+        index = 0
+        while index < len(line):
+            for index_end in range(len(line), index, -1):
+                segment = line[index:index_end]
+                if search(r'^ *log *\($', segment):
+                    index_new = index_end
+                    for index_end in range(len(line), index_new, -1):
+                        segment = line[index_new:index_end]
+                        if segment[-1] != ')':
+                            continue
+                        lexical_analyzed_expression = lexical_analysis_expression(segment[:-1], line_num)
+                        if lexical_analyzed_expression:
+                            lexical_analyzed[-1].append({
+                                'type': 'log',
+                                'value': lexical_analyzed_expression
+                            })
+                            index = index_end
+                            break
 
 
-print(lexical_analysis('''
-log(
-'''))
+    return lexical_analyzed, False
